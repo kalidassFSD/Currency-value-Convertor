8bae884f873e5d3a88797d1aefa57b1942fca1ba
Kalidass G <149503695+kalidassFSD@users.noreply.github.com>
2024-10-16 20:11:45 +0530

    Add files via upload

---
+++ currencyConvertor.js
@@ -0,0 +1,227 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+
+
+ const url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/{endpoint}";
+//const url = "https://data.fixer.io/api/convert?access_key={bcffc167e87f5f7e11c54b25b95652c6}&from=USD&to=INR&amount=100";
+
+const drops = document.querySelectorAll(".drop select");
+
+let msg =document.querySelector(".msg");
+let btn = document.querySelector(".btn");
+let from = document.querySelector(".from-con select");
+let to =document.querySelector(".to-con select");
+
+
+for(let select of drops){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        if(select.name==="from" && currCode==="USD"){
+            newOption.selected="selected";
+        }
+        else if(select.name==="to" && currCode==="INR"){
+            newOption.selected="selected";
+
+        }
+        select.append(newOption);
+        select.scrollTop = select.scrollHeight;
+    }
+    select.addEventListener("change",(evt)=>{
+        chFlag(evt.target);
+
+    });
+}
+
+let chFlag = (element)=>{
+        code =element.value;
+        ch = countryList[code];
+        let img = element.parentElement.querySelector("img");
+        let nSrc = `https://flagsapi.com/${ch}/flat/64.png`;
+        img.src=nSrc;
+}
+
+const chMsg = async()=>{
+    let amt =document.querySelector(".amt input");
+    let amtVal = amt.value;
+    if(amtVal==="" || amtVal<1){
+        amt.value=1;
+        amtVal=1;
+    }
+
+    let newUrl=`https://open.er-api.com/v6/latest/${from.value.toLowerCase()}`;
+    let response = await fetch(newUrl);
+    //console.log(response);
+    let responseData = await response.json();
+    //console.log(responseData.rates);
+    let rate = responseData.rates[to.value];
+    //console.log(rate);
+
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${from.value} = ${finalAmount} ${to.value}`;
+};
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    chMsg();
+   
+});
